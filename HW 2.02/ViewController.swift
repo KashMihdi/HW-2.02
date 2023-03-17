09408b0e89fb777143e13994cf687061b0a92a92
KashMihdi <kashmihdi@gmail.com>
2023-03-17 19:13:02 +0300

    Connection of controller elements and description of application logic

--- HW 2.02/ViewController.swift
+++ HW 2.02/ViewController.swift
@@ -9,10 +9,50 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var redLightView: UIView!
+    @IBOutlet weak var yellowLightView: UIView!
+    @IBOutlet weak var greenLightView: UIView!
+    @IBOutlet weak var startButton: UIButton!
+    
+    private var viewColor: ColorView = .red
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        roundCornersOfView()
+        startButton.layer.cornerRadius = 14
     }
+    
+    
+    @IBAction func startButtonTapped() {
+        startButton.setTitle("NEXT", for: .normal)
+        switch viewColor {
+        case .red:
+            greenLightView.alpha = 0.3
+            redLightView.alpha = 1
+            viewColor = .yellow
+        case .yellow:
+            redLightView.alpha = 0.3
+            yellowLightView.alpha = 1
+            viewColor = .green
+        case .green:
+            yellowLightView.alpha = 0.3
+            greenLightView.alpha = 1
+            viewColor = .red
+        }
+    }
+    
+    private enum ColorView {
+        case red, yellow, green
+    }
+    
+    private func roundCornersOfView() {
+        let radiusOfCorner = redLightView.bounds.width / 2
+        redLightView.layer.cornerRadius = radiusOfCorner
+        yellowLightView.layer.cornerRadius = radiusOfCorner
+        greenLightView.layer.cornerRadius = radiusOfCorner
+    }
+    
+    
 
 
 }
